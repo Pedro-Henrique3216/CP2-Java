6058408138da2efc68db5f21b95e2b41031114dd
Pedro Henrique <ph6704938@gmail.com>
2025-05-06 23:11:27 -0300

    chore: add swagger config

---
+++ src/main/java/br/com/cp2_java/infrastructure/config/OpenAPIConfig.java
@@ -0,0 +1,28 @@
+package br.com.cp2_java.infrastructure.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Product Management API")
+                        .description("API for selling books")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("API Support")
+                                .email("ph6704938@gmail.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0"))
+                );
+    }
+}
